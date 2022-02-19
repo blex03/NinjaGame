d7c38c4eccfa572eee3826255e0579d32b7e1446
Bryan Lecza <brylex0324@gmail.com>
2022-02-19 16:51:04 -0500

    reworked rotation system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,108 +1,78 @@
 using UnityEngine;
 using System;
-using PlayerPos = SpritePositions;
-
 
 public class PlayerMovement : MonoBehaviour {
-    public Rigidbody2D rb2;
+    public Rigidbody2D rb;
+    public Camera cam;
+
     public float speed = 5.0f;
-    private Vector2 movement;
+
     public SpriteRenderer spriteRenderer;
     public Sprite[] spriteArray;
 
-    
-    void Start () {
-        rb2 = GetComponent<Rigidbody2D>();
-    }
+    private Vector2 direction;
+    private Vector2 mousePos;
 
     //Movement
     void moveCharacter(Vector2 direction){
-        rb2.MovePosition((Vector2)transform.position + (direction.normalized * speed * Time.fixedDeltaTime));
+        rb.MovePosition(rb.position + (direction.normalized * speed * Time.fixedDeltaTime));
     }
     
     //Direction
-    private void Direction(){
-        Vector2 mousePos = Input.mousePosition;
+    private void playerDirection(){
+        Vector2 lookDir = mousePos - rb.position;
 
-        float rad = (float)(Math.PI / 180);
-        
-        float relativeX = mousePos.x - PlayerPos.screenPos.x;
-        float relativeY = mousePos.y - PlayerPos.screenPos.y;
-        float ratio;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;    
 
-        bool quad1 = false;
-        bool quad2 = false;
-        bool quad3 = false;
-        bool quad4 = false;
-
-        //Mouse Quadrant with Character at origin
-        if (relativeX > 0 && relativeY > 0) {
-            quad1 = true;
-        }
-        if (relativeX < 0 && relativeY > 0) {
-            quad2 = true;
-        }
-        if (relativeX < 0 && relativeY < 0) {
-            quad3 = true;
-        }
-        if (relativeX > 0 && relativeY < 0) {
-            quad4 = true;
-        }
-
-        if (relativeX != 0) {
-		ratio = relativeY / relativeX;
-        }
-        else {
-            ratio = 0;
-        }
-        
         //S
-        if (ratio <= Math.Tan(292.5 * rad) && quad4 || ratio >= Math.Tan(247.5 * rad) && quad3) {
+        if (angle >= -112.5 && angle <= -67.5) {
             spriteRenderer.sprite = spriteArray[0]; 
         }
 
         //SW
-        if ((ratio < Math.Tan(247.5 * rad) && ratio > Math.Tan(202.5 * rad)) && quad3) {
+        if (angle > -157.5 && angle < -112.5) {
             spriteRenderer.sprite = spriteArray[1]; 
         }
 
         //W
-        if (ratio <= Math.Tan(202.5 * rad) && quad3 || ratio >= Math.Tan(157.5 * rad) && quad2) {
+        if (angle >= 157.5 || angle <= -157.5) {
             spriteRenderer.sprite = spriteArray[2]; 
         }
 
         //NW
-        if ((ratio < Math.Tan(157.5 * rad) && ratio > Math.Tan(112.5 * rad)) && quad2) {
+        if (angle < 157.5 && angle > 112.5) {
             spriteRenderer.sprite = spriteArray[3]; 
         }
         //N
-        if (ratio <= Math.Tan(112.5 * rad) && quad2|| ratio >= Math.Tan(67.5 * rad) && quad1) {
+        if (angle <= 112.5 && angle >= 67.5) {
             spriteRenderer.sprite = spriteArray[4]; 
         }
 
         //NE
-        if ((ratio < Math.Tan(67.5 * rad) && ratio > Math.Tan(22.5 * rad)) && quad1) {
+        if (angle < 67.5  && angle > 22.5) {
             spriteRenderer.sprite = spriteArray[5];
         }
 
         //E
-        if (ratio <= Math.Tan(22.5 * rad) && quad1 || ratio >= Math.Tan(337.5 * rad) && quad4) {
+        if (angle <= 22.5 &&  angle >= -22.5)  {
             spriteRenderer.sprite = spriteArray[6]; 
         }
 
         //SE
-        if ((ratio < Math.Tan(337.5 * rad) && ratio > Math.Tan(292.5 * rad)) && quad4) {
+        if (angle > -67.5  && angle < -22.5) {
             spriteRenderer.sprite = spriteArray[7]; 
         }
         
     }
 
     void Update () {
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
+
     void FixedUpdate(){
-        moveCharacter(movement);
-        Direction();
+        moveCharacter(direction);
+        playerDirection();
     }
 
     
