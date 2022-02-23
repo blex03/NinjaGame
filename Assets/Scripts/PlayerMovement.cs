504ea5ac6dba075d72612edd421c3eccf037bfc5
Bryan Lecza <brylex0324@gmail.com>
2022-02-22 20:29:44 -0500

    omnidirectional attack!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,11 +13,8 @@ public class PlayerMovement : MonoBehaviour {
 
     private Vector2 direction;
     private Vector2 mousePos;
-
-    Vector2 playerPos;
-    Vector2 attackPos; 
+    private Vector2 playerPos;
     
-
     //Movement
     void moveCharacter(Vector2 direction){
         rb.MovePosition(rb.position + (direction.normalized * speed * Time.fixedDeltaTime));
@@ -26,64 +23,61 @@ public class PlayerMovement : MonoBehaviour {
     //Direction
     private void playerDirection(){
         Vector2 lookDir = mousePos - rb.position;
-
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;    
 
+
+        //Attack Direction
+        float circle_size = Mathf.Sqrt(Mathf.Pow(lookDir.x, 2) + Mathf.Pow(lookDir.y, 2));
+	    float circle_factor = circle_size / 1;
+
+        attackPoint.transform.position = new Vector2(playerPos.x + (lookDir.x / circle_factor), playerPos.y + (lookDir.y / circle_factor));
+        
+        if (lookDir.x > 0){
+            attackPoint.transform.rotation = Quaternion.Euler(0, 180, -angle);
+        }
+
+        if (lookDir.x <= 0) {
+            attackPoint.transform.rotation = Quaternion.Euler(0, 0, angle - 180);
+        }
+        
+        //Player Direction
         //S
         if (angle >= -112.5 && angle <= -67.5) {
-            spriteRenderer.sprite = spriteArray[0]; 
-            attackPoint.transform.position = new Vector2(playerPos.x, playerPos.y - 0.9f);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 0, 90);
+            spriteRenderer.sprite = spriteArray[0];        
         }
 
         //SW
         if (angle > -157.5 && angle < -112.5) {
             spriteRenderer.sprite = spriteArray[1]; 
-            attackPoint.transform.position = new Vector2(playerPos.x - 0.636f, playerPos.y - 0.636f);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 0, 55);
         }
 
         //W
         if (angle >= 157.5 || angle <= -157.5) {
             spriteRenderer.sprite = spriteArray[2];
-            attackPoint.transform.position = new Vector2(playerPos.x - 0.9f, playerPos.y);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 0, 0);
         }
 
         //NW
         if (angle < 157.5 && angle > 112.5) {
             spriteRenderer.sprite = spriteArray[3]; 
-            attackPoint.transform.position = new Vector2(playerPos.x - 0.636f, playerPos.y + 0.636f);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 0, 315);
-            
         }
         //N
         if (angle <= 112.5 && angle >= 67.5) {
             spriteRenderer.sprite = spriteArray[4]; 
-            attackPoint.transform.position = new Vector2(playerPos.x, playerPos.y + 0.9f);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 180, 270);
         }
 
         //NE
         if (angle < 67.5  && angle > 22.5) {
             spriteRenderer.sprite = spriteArray[5];
-            attackPoint.transform.position = new Vector2(playerPos.x + 0.636f, playerPos.y + 0.636f);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 180, 315);
         }
 
         //E
         if (angle <= 22.5 &&  angle >= -22.5)  {
             spriteRenderer.sprite = spriteArray[6];
-            attackPoint.transform.position = new Vector2(playerPos.x + 0.9f, playerPos.y);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 180, 0);
-            
         }
 
         //SE
         if (angle > -67.5  && angle < -22.5) {
             spriteRenderer.sprite = spriteArray[7]; 
-            attackPoint.transform.position = new Vector2(playerPos.x + 0.636f, playerPos.y - 0.636f);
-            attackPoint.transform.rotation = Quaternion.Euler(0, 180, 55);
         }
         
     }
@@ -93,7 +87,6 @@ public class PlayerMovement : MonoBehaviour {
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         playerPos = transform.position;
-        attackPos = attackPoint.position;
     }
 
     void FixedUpdate(){
